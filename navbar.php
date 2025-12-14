9b9db5e8ca1b73ada52ff7f2f9db69c09dd0fd0a
GUILDER <pplg39395@gmail.com>
2025-12-14 08:59:43 -0400

    Ya no se el error en la parte del nabvar

--- navbar.php
+++ navbar.php
@@ -2,10 +2,6 @@
 // ===== NAVBAR INCLUIBLE PARA TODAS LAS PÁGINAS =====
 // Incluir en todas las páginas: <?php include 'navbar.php'; ?>
 
-// Incluir configuración y funciones
-require_once __DIR__ . '/config/database.php';
-?>
-
 <!-- Font Awesome para iconos -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="assets/css/social-media.css">
